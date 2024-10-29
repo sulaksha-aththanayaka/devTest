508f69e18e0a60920f84d8575210cfc5650a589c
sulaksha-aththanayaka <sulakshaa04@gmail.com>
2024-10-29 20:54:41 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <div className='flex justify-center items-center h-[90vh] bg-green-400 m-10'>
+        <h1 className='bg-red-400 text-4xl p-5'>Hello World!</h1>
+      </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
