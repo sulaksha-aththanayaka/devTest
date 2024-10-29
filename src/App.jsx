3b329270cba6178b5dc49f81e90f4592157efa3e
sulaksha-aththanayaka <sulakshaa04@gmail.com>
2024-10-30 01:23:32 +0530

    eleventh change

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
     <div>
-      <div className='flex justify-center items-center h-[90vh] bg-green-400 m-10'>
-        <h1 className='bg-red-400 text-4xl p-5'>Hello World 10!</h1>
+      <div className='flex justify-center items-center h-[90vh] bg-blue-400 m-10'>
+        <h1 className='bg-red-400 text-4xl p-5'>Hello Jenkins!</h1>
       </div>
     </div>
       
